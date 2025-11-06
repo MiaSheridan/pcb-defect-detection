2c8e13f47c04b3f60e32f751a52f4173c2c91c8b
SheridanVLopez <SheridanVLopez@mac.home>
2025-11-06 00:30:35 -0500

    optimizing

--- code/regularized_training.py
+++ code/regularized_training.py
@@ -125,11 +125,14 @@ def train_smart_simple():
     
     model = tf.keras.Sequential([
         tf.keras.layers.Conv2D(32, (3,3), activation='relu', input_shape=(64,64,3)),
+        tf.keras.layers.BatchNormalization(), 
         tf.keras.layers.MaxPooling2D(2,2),
         tf.keras.layers.Conv2D(64, (3,3), activation='relu'),
+        tf.keras.layers.BatchNormalization(),
         tf.keras.layers.MaxPooling2D(2,2),
 
         tf.keras.layers.Conv2D(64, (3,3), activation='relu'),
+        tf.keras.layers.BatchNormalization(),
         tf.keras.layers.Flatten(),
         tf.keras.layers.Dense(128, activation='relu'),
         tf.keras.layers.Dropout(0.5),
