0d5a08fdd257b7228c5e2bfe35b945a5603378ec
SheridanVLopez <SheridanVLopez@mac.home>
2025-11-07 18:37:52 -0500

    Adding ui for PCB defect identification

--- code/app.py
+++ code/app.py
@@ -7,7 +7,7 @@ import io
 app = Flask(__name__)
 
 #Load trained model
-model = load_model('models/smart_simple_model.h5')
+model = load_model('../models/smart_simple_model.h5')
 
 #The 6 defect types 
 DEFECT_TYPES = [
