f195556bfbae222c1e1e93b9750637b0c2340557
SheridanVLopez <SheridanVLopez@mac.home>
2025-11-07 16:03:18 -0500

    optimizing

--- code/regularized_training.py
+++ code/regularized_training.py
@@ -4,6 +4,7 @@ import os
 import tensorflow as tf
 import cv2
 import numpy as np
+from tensorflow.keras import regularizers
 
 def create_better_dataset():
     """ EXACT same dataset function"""
@@ -128,17 +129,17 @@ def train_smart_simple():
     
     model = tf.keras.Sequential([
         tf.keras.layers.Conv2D(32, (3,3), activation='relu', input_shape=(128,128,3)),
-       
         tf.keras.layers.MaxPooling2D(2,2),
+
         tf.keras.layers.Conv2D(64, (3,3), activation='relu'),
-        
         tf.keras.layers.MaxPooling2D(2,2),
 
         tf.keras.layers.Conv2D(64, (3,3), activation='relu'),
         
         
         tf.keras.layers.Flatten(),
-        tf.keras.layers.Dense(128, activation='relu'),
+        tf.keras.layers.Dense(128, activation='relu', 
+            kernel_regularizer=regularizers.l2(0.001)),
         tf.keras.layers.Dropout(0.5),
         tf.keras.layers.Dense(6, activation='softmax')
     ])
